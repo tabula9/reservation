4a60fceb9af7ba333c4842ad56f2c40f2c393bb6
tabula9 <fhmtt484@gmail.com>
2023-06-27 12:26:31 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'top/search'
+  get "users/account" => "users#account"
+  get "users/profile" => "users#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'top#search'
 end
