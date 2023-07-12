2d53ff34404939b41ddbee114d18fb3fec8b099a
tabula9 <fhmtt484@gmail.com>
2023-07-12 23:13:34 +0900

    css調整中。topページ、userモデル関連のページは完了

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,14 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %W(
+    application.css
+    common.scss
+    reservations.scss
+    rooms.scss
+    header.css
+    top.sccs
+    devise.css
+    user.css
+)
